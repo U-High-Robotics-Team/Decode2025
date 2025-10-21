dd7847c4f71e9b8bd428ddd639b5e8442d6c5b07
Ashrith Ponna <156397382+AshrithPonna@users.noreply.github.com>
2025-10-21 13:28:57 -0500

    Create HuskyLens_IT1.java for the integration of HuskyLens capabilities.
    Add HuskyLens_IT1.java for basic program infrastructure and object references.

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/HuskyLens_IT1.java
@@ -0,0 +1,18 @@
+package org.firstinspires.ftc.robotcontroller.external.samples; // Referencing the parent package which this program will classify into.
+
+// Necessary library imports for lens functionality in java.
+
+import com.qualcomm.hardware.dfrobot.HuskyLens; 
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.hardware.HardwareMap;
+
+@TeleOp(name = "Sensor: HuskyLens", group = "Sensor") // Creating a teleop reference.
+@Disabled // Negating teleop functionality.
+
+public class HuskyLensMetrics extends LinearOpMode { // Creating a class extension to generalized sequential system.
+  
+
+
+
