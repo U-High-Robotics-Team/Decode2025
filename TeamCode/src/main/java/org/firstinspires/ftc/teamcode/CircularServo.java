11119046c6d006875d15785a0f8e2bdb880d49fd
Ashrith Ponna <156397382+AshrithPonna@users.noreply.github.com>
2025-10-31 11:19:36 -0500

    Refactor CircularServo to RotationServo with corrections
    Fixed syntax errors and updated class name for clarity.

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/CircularServo.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/CircularServo.java
@@ -1,6 +1,28 @@
 package org.firstinspires.ftc.teamcode; // Main library that allows for easy access of other methodological programs in this folder.
 
-import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode; 
+// Necessary library imports for lens functionality in java.
+
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;  
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 import com.qualcomm.robotcore.hardware.Servo;
-import com.qualcomm.robotcore.util.Range
+import com.qualcomm.robotcore.util.Range;
+
+@TeleOp(name = "RotationServo") // Creating an accessible group name that can characterize the program.
+  
+public class RotationServo extends LinearOpMode { // Initializing a program that is inclusive of other methods.
+
+  private Servo = rotationServo; // Initializing a servo instance.
+
+  @Override // Progressing program past a "LinearOpMode" extension.
+
+  public void runOpMode(){ // Creating a method that contains the majority of sub-methods.
+
+    rotationServo = hardwareMap.get(Servo.class, "???"); // Getting servo information from control center. \NAME REQUIRED/
+    
+    // Updating control system to contain validating information.
+    
+    telemetry.addData("Status", "Initialized");
+    telemetry.update(); 
+
+    
+    
