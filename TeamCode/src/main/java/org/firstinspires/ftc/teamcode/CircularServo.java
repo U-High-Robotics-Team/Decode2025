a823c4e437bbe41427ecf8103ce3026bcde7351d
Ashrith Ponna <156397382+AshrithPonna@users.noreply.github.com>
2025-10-31 11:10:09 -0500

    Add CircularServo.java with package and imports
    This file includes package declaration and imports necessary for controlling a servo in a robot.

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/CircularServo.java
@@ -0,0 +1,6 @@
+package org.firstinspires.ftc.teamcode; // Main library that allows for easy access of other methodological programs in this folder.
+
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode; 
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.hardware.Servo;
+import com.qualcomm.robotcore.util.Range
