ec4bed1ed12c687b1890c2a72cb007c9defcbec2
Ashrith Ponna <156397382+AshrithPonna@users.noreply.github.com>
2025-11-18 16:10:35 -0600

    Implement Teleop class for Limelight integration

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/LimeLight3AMetrics.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/LimeLight3AMetrics.java
@@ -1,1 +1,33 @@
+// SAMPLE
 
+public class Teleop extends LinearOpMode {
+
+    private Limelight3A limelight;
+
+    @Override
+    public void runOpMode() throws InterruptedException
+    {
+        limelight = hardwareMap.get(Limelight3A.class, "limelight");
+
+        telemetry.setMsTransmissionInterval(11);
+
+        limelight.pipelineSwitch(0);
+
+        /*
+         * Starts polling for data.
+         */
+        limelight.start();
+        
+        while (opModeIsActive()) {
+            LLResult result = limelight.getLatestResult();
+            if (result != null) {
+                if (result.isValid()) {
+                    Pose3D botpose = result.getBotpose();
+                    telemetry.addData("tx", result.getTx());
+                    telemetry.addData("ty", result.getTy());
+                    telemetry.addData("Botpose", botpose.toString());
+                }
+            }
+        }
+    }
+}
